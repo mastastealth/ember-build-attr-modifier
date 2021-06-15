8159e272610431ae58c9126bf37b477c5718567e
Brian Franco <mastastealth+git@gmail.com>
2021-06-15 14:30:03 -0400

    Assure webpack 5

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,8 +5,8 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 module.exports = function (defaults) {
   let app = new EmberAddon(defaults, {});
 
-  const { maybeEmbroider } = require('@embroider/test-setup');
-  return maybeEmbroider(app, {
+  const { Webpack } = require('@embroider/webpack');
+  return require('@embroider/compat').compatBuild(app, Webpack, {
     staticAddonTestSupportTrees: true,
     staticAddonTrees: true,
     staticHelpers: true,
