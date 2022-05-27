ac7d809d0928882c76b955f52096cdde8b93c724
Brian Franco <mastastealth+git@gmail.com>
2022-05-27 12:15:43 -0400

    Ember 3.28

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,11 +5,12 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 module.exports = function (defaults) {
   let app = new EmberAddon(defaults, {});
 
-  const { Webpack } = require('@embroider/webpack');
-  return require('@embroider/compat').compatBuild(app, Webpack, {
-    staticAddonTestSupportTrees: true,
-    staticAddonTrees: true,
-    staticHelpers: true,
-    staticComponents: true,
+  const { maybeEmbroider } = require('@embroider/test-setup');
+  return maybeEmbroider(app, {
+    skipBabel: [
+      {
+        package: 'qunit',
+      },
+    ],
   });
 };
